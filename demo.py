6f192f4966a34ea400ca263c8c68a5a5f7d9de8c
Alejandro Suarez <asuarez@iri.upc.edu>
2019-12-03 20:15:15 +0100

    added demo script

--- demo.py
+++ demo.py
@@ -1,12 +1,25 @@
+#!/usr/bin/env python3
+
 from z3 import *
 
 
-def setProblem():
-    pass
+def setProblem(traces):
+    predicates = set()
+    objects = set()
+    for trace in traces:
+        for state in trace:
+            for predicate in state:
+                predicates.add((predicate[0], len(predicate)-1))
+                for obj in predicate:
+                    objects.add(obj)
+    for pred,arity in predicates:
+        print("{}/{}".format(pred,arity))
+    print(objects)
 
 
 def main():
-    pass
+    from example_traces import traces
+    setProblem(traces)
 
 
 if __name__ == "__main__":
---
+++ example_traces.py
@@ -0,0 +1,11 @@
+
+traces = [
+    [
+        { ("on", "a", "b"), ("on", "b", "c"), ("ontable", "c"), ("clear", "a") },
+        { ("ontable", "a"), ("on", "b", "c"), ("ontable", "c"), ("clear", "a"), ("clear", "b") },
+        { ("ontable", "a"), ("on", "b", "a"), ("ontable", "c"), ("clear", "b"), ("clear", "c") },
+        { ("ontable", "a"), ("on", "b", "a"), ("on", "c", "b"), ("clear", "c") }
+    ]
+]
+
+

27188492a97df7ebcef94fea9c211f198b42b9d1
Alejandro Suarez <asuarez@iri.upc.edu>
2019-12-04 14:29:57 +0100

    generate all predicates

--- demo.py
+++ demo.py
@@ -2,19 +2,30 @@
 
 from z3 import *
 
+from itertools import permutations
+
+
+def generateAllPredicates(predicate_signatures, objects):
+    predicates = []
+    for pred, arity in predicate_signatures:
+        for args in permutations(objects, arity):
+            predicates.append((pred, *args))
+    return predicates
+
 
 def setProblem(traces):
-    predicates = set()
+    predicate_signatures = set()
     objects = set()
     for trace in traces:
         for state in trace:
             for predicate in state:
-                predicates.add((predicate[0], len(predicate)-1))
-                for obj in predicate:
+                predicate_signatures.add((predicate[0], len(predicate)-1))
+                for obj in predicate[1:]:
                     objects.add(obj)
-    for pred,arity in predicates:
+    for pred,arity in predicate_signatures:
         print("{}/{}".format(pred,arity))
     print(objects)
+    print(generateAllPredicates(predicate_signatures, objects))
 
 
 def main():

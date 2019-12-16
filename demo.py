4c4c0432c9cf1173a5973f8b9b5181f68cf8d749
Alejandro Suarez <asuarez@iri.upc.edu>
2019-12-16 19:30:54 +0100

    solution vector

--- demo.py
+++ demo.py
@@ -30,18 +30,30 @@ def extractInfo(traces):
     return predicate_signatures, static_predicates, objects
 
 
+def setProblem(traces, action_v, timesteps):
+    predicate_signatures, static_predicates, objects = extractInfo(traces)
+    max_arity = max(action_v)
+    solution = []
+    for action_idx, action_arity in enumerate(action_v):
+        solution.append({})
+        for predicate, pred_arity in predicate_signatures:
+            for section in ("pre", "add", "del"):
+                for slots in permutations(range(action_arity), pred_arity):
+                    var_name = "a{:02}_{}_{}".format(action_idx, section, predicate)
+                    if slots:
+                        var_name += "_" + "_".join("slot{}".format(s) for s in slots)
+                    solution[action_idx][var_name] = Bool(var_name)
+    for action_idx, action in enumerate(solution):
+        print("a{:02}:".format(action_idx))
+        print("  "+"\n  ".join(sorted(action.keys())))
+
+    # for predicate in generateAllPredicates(predicate_signatures, objects):
+        # pass
+
+
 def main():
     from example_traces import traces
-    predicate_signatures, static_predicates, objects = extractInfo(traces)
-    print("Predicate signatures")
-    for pred,arity in predicate_signatures:
-        print("{}/{}".format(pred,arity))
-    print("Static predicates")
-    for pred,arity in static_predicates:
-        print("{}/{}".format(pred,arity))
-
-    print(objects)
-    print(generateAllPredicates(predicate_signatures, objects))
+    setProblem(traces, [2, 2, 3], 10)
 
 
 if __name__ == "__main__":

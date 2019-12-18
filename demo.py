7c71a0c325019e58330a50729f21d1356de6c91a
Alejandro Suarez <asuarez@iri.upc.edu>
2019-12-18 18:41:33 +0100

    include trace id in variable name

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@
 
 from z3 import *
 
-from itertools import permutations
+from itertools import permutations, product
 
 
 def generateAllPredicates(predicate_signatures, objects):
@@ -30,15 +30,19 @@ def extractInfo(traces):
     return predicate_signatures, static_predicates, objects
 
 
-def solvariable(action_idx, section, predicate, *slots):
+def solvar(action_idx, section, predicate, *slots):
     var_name = "a{:02}_{}_{}".format(action_idx, section, predicate)
     if slots:
         var_name += "_" + "_".join("slot{}".format(s) for s in slots)
     return var_name
 
 
-# def slotvariable(slot_idx, obj, time_step):
-    # return "slot{}_{}_t{:02}".format(slot_idx, obj
+def slotvar(slot_idx, obj, trace_idx, time_step):
+    return "slot{}_{}_tr{:02}_ts{:02}".format(slot_idx, obj, trace_idx, time_step)
+
+
+def actvar(action_idx, trace_idx, time_step):
+    return "exec_a{:02}_tr{:02}_ts{:02}".format(action_idx, trace_idx, time_step)
 
 
 def setProblem(traces, action_v, timesteps):
@@ -46,24 +50,37 @@ def setProblem(traces, action_v, timesteps):
     max_arity = max(action_v)
 
     # generate solution variables
-    solution = []
+    solution = [{} for _ in range(len(action_v))]
     for action_idx, action_arity in enumerate(action_v):
-        solution.append({})
         for predicate, pred_arity in predicate_signatures:
             for section in ("pre", "add", "del"):
                 for slots in permutations(range(action_arity), pred_arity):
-                    var_name = "a{:02}_{}_{}".format(action_idx, section, predicate)
-                    if slots:
-                        var_name += "_" + "_".join("slot{}".format(s) for s in slots)
+                    var_name = solvar(action_idx, section, predicate, *slots)
                     solution[action_idx][var_name] = Bool(var_name)
     for action_idx, action in enumerate(solution):
         print("a{:02}:".format(action_idx))
         print("  "+"\n  ".join(sorted(action.keys())))
 
+    # generate action variables (one per timestep)
+    action_vars = {}
+    for idx, tr, ts in product(range(len(action_v)), range(len(traces)), range(timesteps)):
+        var_name = actvar(idx, tr, ts)
+        action_vars[var_name] = Bool(var_name)
+    print(sorted(action_vars.keys()))
+
+    # generate slot variables
+    slot_variables = {}
+    for slot_idx, obj, tr, ts in product(range(max_arity), objects, range(len(traces)), range(timesteps)):
+        var_name = slotvar(slot_idx, obj, tr, ts)
+        slot_variables[var_name] = Bool(var_name)
+    print(sorted(slot_variables.keys()))
+
+    # for section in ("pre", "add"):
+        # for (predicate, pred_arity), tr, ts in product(predicate_signatures, range(len(traces)), range(timesteps)):
+            # for action_idx, act_arity in enumerate(action_v):
 
 
-    # for predicate in generateAllPredicates(predicate_signatures, objects):
-        # pass
+
 
 
 def main():
